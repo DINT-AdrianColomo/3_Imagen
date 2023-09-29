703d8dd23a9f64d6843e23bcf1f138d3bbdb110c
ADRIÁN COLOMO MORAGUES <adrcolmor@alu.edu.gva.es>
2023-09-29 13:58:51 +0200

    Agregar archivos de proyecto.

---
+++ 3_Imagen/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3_Imagen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AltaButton_Checked(object sender, RoutedEventArgs e)
+        {
+            starWarsImage.Opacity = 1;
+        }
+
+        private void BajaButton_Checked(object sender, RoutedEventArgs e)
+        {
+            starWarsImage.Opacity = 0.3;
+        }
+
+        private void MediaButton_Checked(object sender, RoutedEventArgs e)
+        {
+            starWarsImage.Opacity = 0.6;
+        }
+
+        private void FillButton_Checked(object sender, RoutedEventArgs e)
+        {
+            starWarsImage.Stretch = Stretch.Fill;
+        }
+
+        private void UniformButton_Checked(object sender, RoutedEventArgs e)
+        {
+            starWarsImage.Stretch=Stretch.Uniform;
+        }
+
+        private void UniformToFillButton_Checked(object sender, RoutedEventArgs e)
+        {
+            starWarsImage.Stretch=Stretch.UniformToFill;
+        }
+
+        private void NoneButton_Checked(object sender, RoutedEventArgs e)
+        {
+            starWarsImage.Stretch = Stretch.None;
+        }
+    }
+}
